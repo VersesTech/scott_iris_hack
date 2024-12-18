faa5ec5b7bf4b34d2c99b48c3f7bed878d34137b
Scott Carroll 🐙 <scott.carroll@verses.ai>
2024-12-18 13:33:20 -0500

    Tuning after an attempt at a first run

---
+++ pull_logs_from_login_node_to_scotts_workspace.sh
@@ -0,0 +1,2 @@
+rsync -avz --progress -e ssh scott@ec2-34-207-230-166.compute-1.amazonaws.com:/home/scott/github/VersesTech/scott_iris_hack/scripts/baselines/training/*.err /Users/scottcarroll/github/VersesTech/scott_iris_hack/logs_from_cluster/
+rsync -avz --progress -e ssh scott@ec2-34-207-230-166.compute-1.amazonaws.com:/home/scott/github/VersesTech/scott_iris_hack/scripts/baselines/training/*.out /Users/scottcarroll/github/VersesTech/scott_iris_hack/logs_from_cluster/
--- scripts/baselines/training/start_all_training.sh
+++ scripts/baselines/training/start_all_training.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname "$0")
 for file in train_*.sbatch; do
-  echo "dry run, not actually running sbatch -G 1 $file"
-#  sbatch -G 1 "$file"
+  echo "running sbatch -G 1 $file"
+  sbatch -G 1 "$file"
 done
