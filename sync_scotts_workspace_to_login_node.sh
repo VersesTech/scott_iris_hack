7d11e63f6c5345f6c9fa6ff62e7dcf0325286af2
Scott Carroll 🐙 <scott.carroll@verses.ai>
2024-12-18 13:18:41 -0500

    Protect from accidentally overwriting cluster code

--- sync_scotts_workspace_to_login_node.sh
+++ sync_scotts_workspace_to_login_node.sh
@@ -1,2 +1,2 @@
-rsync -avz --progress -e ssh --delete --exclude='outputs' --exclude='iris_env_scott_local_3_9_20' --exclude='iris_env_scott_cluster_loginnode_3_9_20' /Users/scottcarroll/github/VersesTech/scott_iris_hack/ scott@ec2-34-207-230-166.compute-1.amazonaws.com:/home/scott/github/VersesTech/scott_iris_hack/
+rsync -avz --progress -e ssh --delete --exclude='outputs' --exclude='iris_env_scott_local_3_9_20' --exclude='iris_env_scott_cluster_loginnode_3_9_20' /Users/scottcarroll/github/VersesTech/scott_iris_hack/ scott@ec2-34-207-230-166.compute-1.amazonaws.com:/home/scott/github/VersesTech/scott_iris_hack_PROTECT_THIS/
 
